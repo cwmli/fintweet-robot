697d99447920e890a1a4cff4f3a5620ca3049028
Calvin Li <calvinl9331@gmail.com>
2021-05-07 22:45:31 -0400

    Allow jobs to run without proxy (#1)

--- jobs.py
+++ jobs.py
@@ -20,12 +20,14 @@ class ScrapeJob(ABC):
     :param proxy_list: list of proxies to shuffle from
     :param rec_plist: recommended proxies to first shuffle from
     :param lock: lock for multithreaded or multiprocessed use case; used during webdriver init
+    :param headless: headless mode
     """
 
-    def __init__(self, scrape_options, proxy_list, rec_plist = [], lock = None):
+    def __init__(self, scrape_options, proxy_list = [], rec_plist = [], lock = None, headless = True):
         random.seed(datetime.datetime.now())
 
         self._webdriver_lock = lock
+        self._headless = headless
 
         self._logger = logging.getLogger(__name__)
 
@@ -34,7 +36,14 @@ class ScrapeJob(ABC):
 
         self._n_plist = len(self._plist)
         self._n_rec_plist = len(self._rec_plist)
-        self._retry_limit = self._n_plist + self._n_rec_plist
+
+        self._use_proxies = self._n_plist + self._n_rec_plist > 0
+
+        if self._use_proxies:
+            self._retry_limit = self._n_plist + self._n_rec_plist
+        else:
+            # Default to 3 retries without proxies
+            self._retry_limit = 3
 
         self._v_plist = []
 
@@ -49,19 +58,21 @@ class ScrapeJob(ABC):
         done = False
         while retries < self._retry_limit and not done:
             try:
-                if retries > self._n_rec_plist - 1:
-                    proxy = self._plist[retries - self._n_rec_plist]
+
+                if self._use_proxies:
+                    if retries > self._n_rec_plist - 1:
+                        proxy = self._plist[retries - self._n_rec_plist]
+                    else:
+                        proxy = self._rec_plist[retries]
+                    self._logger.info("Using proxy: %s for %s" % (proxy, self._scrape_options["symbol"]))
                 else:
-                    proxy = self._rec_plist[retries]
-                self._logger.info("Using proxy: %s for %s" % (proxy, self._scrape_options["symbol"]))
+                    proxy = None
 
-                if self._webdriver_lock:
-                    self._webdriver_lock.acquire()
+                self._try_lock()
 
-                t = self._chrome_driver(proxy=proxy)
+                t = self._chrome_driver(self._headless, proxy=proxy)
 
-                if self._webdriver_lock:
-                    self._webdriver_lock.release()
+                self._try_unlock()
 
                 self._scrape_method(t, self._scrape_options)
                 done = True
@@ -84,6 +95,14 @@ class ScrapeJob(ABC):
     def _scrape_method(self, driver, kwargs):
         pass
 
+    def _try_lock(self):
+        if self._webdriver_lock:
+            self._webdriver_lock.acquire()
+
+    def _try_unlock(self):
+        if self._webdriver_lock:
+            self._webdriver_lock.release()
+
     def _chrome_driver(self, headless=True, proxy=None, timeout=30):
 
         driver_path = chromedriver_autoinstaller.install()
@@ -105,8 +124,6 @@ class ScrapeJob(ABC):
             opts.add_argument('--disable-gpu')
             opts.add_argument('--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36')
             opts.headless = True
-        else:
-            opts.add_extension("./ublock1.34.0.crx")
 
         opts.add_argument('log-level=3')
 
